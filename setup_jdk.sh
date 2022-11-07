da4d206030500e1a593016d6f1d3d62a09d7a82d
Jay <jamied_uk@hotmail.com>
2022-11-07 15:49:27 +0000

    Update setup_jdk.sh

--- setup_jdk.sh
+++ setup_jdk.sh
@@ -5,15 +5,4 @@
 # ./setp_jdk.sh
 #
 echo "Setting Up Java JDK"
-sudo su -
-
-#wget https://download.oracle.com/java/17/latest/jdk-17_linux-x64_bin.rpm
-#rpm -ivh jdk-17_linux-x64_bin.rpm
-
-sudo apt update
-
-sudo apt install -y libc6-x32 libc6-i386
-
-wget https://download.oracle.com/java/17/latest/jdk-17_linux-x64_bin.deb
-
-sudo dpkg -i jdk-17_linux-x64_bin.deb
+sudo apt install -y ghidra
